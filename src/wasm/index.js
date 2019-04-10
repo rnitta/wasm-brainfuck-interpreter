fd64b0b1275c4eb46d9342e04dfaa7c9b2c071a7
rnitta <attinyes@gmail.com>
2019-04-10 19:00:41 +0900

    no message

--- src/wasm/index.js
+++ src/wasm/index.js
@@ -1,6 +1,6 @@
 const wasm = import('./optimized.js');
 wasm.then(module => {
-  const { add1, retstr } = module;
+  const { add1, retstr } = modlue;
   document.getElementById('websystestbtn').addEventListener('click', () => {
     console.log(add1(1));
     console.log(retstr('a'));
